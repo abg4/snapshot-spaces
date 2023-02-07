c42828af5c8fc8910bce18d9e011d88bac022d16
Alex Gaines <againes@umaproject.org>
2023-02-07 11:29:01 -0600

    Initial commit
    Signed-off-by: Alex Gaines <againes@umaproject.org>

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # snapshot-spaces
+
+Simple script that retrieves Snapshot spaces with data.
+
+## Install packages
+
+```bash
+yarn
+```
+
+## Run script
+
+```bash
+node index.js
+```
+
+Helpful tool to convert to csv file: https://www.convertcsv.com/json-to-csv.htm
---
+++ index.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+import fs from "fs";
+
+async function getSnapshotData() {
+  let skip = 0;
+  let spaces;
+  const allSpaces = [];
+
+  const getContractAddress = (chain, address) =>
+    Number(chain) === 1
+      ? "https://etherscan.io/address/" + address
+      : Number(chain) === 137
+      ? "https://polygonscan.com/address/" + address
+      : Number(chain) === 100
+      ? "https://gnosisscan.io/address/" + address
+      : Number(chain) === 56
+      ? "https://bscscan.com/address/" + address
+      : address;
+
+  do {
+    spaces = await axios({
+      url: "https://hub.snapshot.org/graphql",
+      method: "post",
+      data: {
+        query: `
+            query Spaces {
+              spaces(first: 1000, skip: ${skip}, orderBy: "proposalsCount", orderDirection: asc) {
+                id
+                name
+                symbol
+                treasuries {
+                  name
+                  address
+                  network
+                }
+                website
+                twitter
+                github
+                plugins
+                proposalsCount
+              }
+            }
+              `,
+      },
+    }).then((result) =>
+      result.data.data.spaces.map((space) => {
+        space?.treasuries?.map((dao) => {
+          space.treasuryNetwork = dao.network;
+          space.treasuryContract = getContractAddress(dao.network, dao.address);
+        });
+        space?.plugins?.safeSnap?.safes?.map((dao) => {
+          if (
+            dao.network === "CHAIN_ID" ||
+            Number(dao.network) === 4 ||
+            Number(dao.network) === 5
+          ) {
+            return;
+          }
+          space.realityNetwork = dao.network || "";
+          space.realityAddress =
+            getContractAddress(dao.network, dao.realityAddress) || "";
+        });
+        return allSpaces.push({
+          name: space.name,
+          spaceUrl: "https://snapshot.org/#/" + space.id,
+          website: space.website !== null ? space.website : "",
+          twitter: space.twitter !== null ? space.twitter : "",
+          github:
+            space.github !== null ? "https://github.com/" + space.github : "",
+          treasuryNetwork: space.treasuryNetwork || "",
+          treasuryContract: space.treasuryContract || "",
+          realityNetwork: space.realityNetwork || "",
+          realityAddress: space.realityAddress || "",
+          proposalCount: space.proposalsCount,
+        });
+      })
+    );
+    skip += 1000;
+  } while (spaces.length > 0);
+
+  fs.mkdirSync("outputs", { recursive: true });
+  fs.writeFileSync(`outputs/spaces.json`, JSON.stringify(allSpaces, null, 2));
+}
+
+getSnapshotData().catch((err) => console.log(err));
